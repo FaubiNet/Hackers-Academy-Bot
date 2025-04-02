7078aa765593314510edb501f9fed8d05cadab4a
FaubiNet <blessingfaustin1206@gmail.com>
2025-04-02 16:31:50 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  botName: "Hackers Academy Bot",  // Nom du bot
+  botOwner: 'Hackers Academy', // Le nom du propriétaire du bot
+  ownerNumber: '+243980418067', // Numéro du propriétaire
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // Clé API pour Giphy (si utilisée)
+  commandMode: "public",  // Mode de commande, public ou privé
+  description: "C'est un bot pour gérer les commandes de groupe et automatiser des tâches.", // Description du bot
+  version: "1.0.0",  // Version du bot
 };
 
 module.exports = settings;
